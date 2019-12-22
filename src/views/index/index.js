e9ac1107b3197760cb5108bd948ae97a57b1d528
wliw <w_angliwei@126.com>
2019-12-22 23:19:17 +0800

    修改全局样式

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -10,18 +10,24 @@ window.addEventListener('click', function (event) {
     console.log(1, event);
 }, false);
 
-$('js-index').addEventListener('click', (event) => {
-    event.stopPropagation();
+let eventTarget = new EventTarget();
+let event = new CustomEvent('myEvent', {
+    detail: 'test'
+});
 
-    import(
-        /* webpackChunkName: 'other' */
-        /* webpackMode: 'lazy' */
-        './other.js'
-    ).then(({ a }) => {
-        console.log('import');
-        console.log(a);
-    });
-}, true);
+eventTarget.addEventListener('myEvent', () => {
+    console.log('trigger cutom event');
+}, false);
+
+$('js-index').addEventListener('click', handleClick, false);
+$('js-index').addEventListener('click', handleClick, false);
+
+function handleClick () {
+    // event = event || window.event;
+    // event.stopPropagation();
+    console.log('click');
+    eventTarget.dispatchEvent(event);
+}
 
 const promise = new Promise((resolve) => {
     setTimeout(() => {
